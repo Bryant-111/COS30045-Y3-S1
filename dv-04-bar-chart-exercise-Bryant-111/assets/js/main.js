bafeccdc167cf0fa62f254bed1dd8fd9d130a193
Pasan DeZoyza <104760094@student.swin.edu.au.com>
2026-04-20 23:27:33 +1000

    4.3

--- dv-04-bar-chart-exercise-Bryant-111/assets/js/main.js
+++ dv-04-bar-chart-exercise-Bryant-111/assets/js/main.js
@@ -1,15 +1,12 @@
+const svg = d3.select(".responsive-svg-container")
+  .append("svg")
+  .attr("viewBox", "0 0 1200 1600")
+  .style("border", "1px solid black");
 
-d3.select("h1")
-  .style("color", "green");
-
-d3.select(".container")
-  .append("p")
-  .text("Purchasing a low energy consumption TV will help with your energy bills!");
-
-d3.select("svg")
+svg
   .append("rect")
-  .attr("x", 50)
-  .attr("y", 50)
-  .attr("width", 100)
-  .attr("height", 30)
-  .style("fill", "green");
+  .attr("x", 10)
+  .attr("y", 10)
+  .attr("width", 414)
+  .attr("height", 16)
+  .attr("fill", "blue");
