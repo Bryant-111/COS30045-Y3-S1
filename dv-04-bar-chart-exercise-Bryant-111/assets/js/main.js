0037f9e5b5ab4d85258c1a2f54984ccf4ea31c4e
Pasan DeZoyza <104760094@student.swin.edu.au.com>
2026-04-21 15:22:13 +1000

    wbfi

--- dv-04-bar-chart-exercise-Bryant-111/assets/js/main.js
+++ dv-04-bar-chart-exercise-Bryant-111/assets/js/main.js
@@ -1,31 +1,3 @@
-console.log("MAIN JS LOADED");
-
-const svgWidth = 500;
-const svgHeight = 800;
-
-const svg = d3.select(".responsive-svg-container")
-  .append("svg")
-  .attr("viewBox", `0 0 ${svgWidth} ${svgHeight}`)
-  .style("border", "1px solid black");
-
-d3.csv("data/tvBrandCount.csv", d => {
-  return {
-    brand: d.brand,
-    count: +d.count
-  };
-}).then(data => {
-  console.log("Loaded data:", data);
-  console.log("Length:", data.length);
-  console.log("Max:", d3.max(data, d => d.count));
-  console.log("Min:", d3.min(data, d => d.count));
-  console.log("Extent:", d3.extent(data, d => d.count));
-
-  data.sort((a, b) => b.count - a.count);
-  console.log("Sorted data:", data);
-
-  createBarChart(data);
-});
-
 const createBarChart = data => {
   console.log("createBarChart called");
 
@@ -47,7 +19,7 @@ const createBarChart = data => {
   barAndLabel
     .append("rect")
     .attr("class", d => `bar bar-${d.count}`)
-    .attr("x", 0)
+    .attr("x", 100)
     .attr("y", 0)
     .attr("width", d => xScale(d.count))
     .attr("height", yScale.bandwidth())
@@ -57,9 +29,9 @@ const createBarChart = data => {
     .append("text")
     .text(d => d.brand)
     .attr("x", 90)
-    .attr("y", 15)
+    .attr("y", yScale.bandwidth() / 2)
+    .attr("dy", "0.35em")
     .attr("text-anchor", "end")
     .style("font-size", "13px");
 };
-
 
