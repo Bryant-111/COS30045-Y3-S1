987d89ce9755ea1797377b61af988ca1b2964939
Pasan DeZoyza <104760094@student.swin.edu.au.com>
2026-04-21 14:27:45 +1000

    4.55

--- dv-04-bar-chart-exercise-Bryant-111/assets/js/main.js
+++ dv-04-bar-chart-exercise-Bryant-111/assets/js/main.js
@@ -1,20 +1,31 @@
-const drawBarChart = data => {
-  console.log("drawBarChart called");
+const svg = d3.select(".responsive-svg-container")
+  .append("svg")
+  .attr("viewBox", "0 0 1200 1600")
+  .style("border", "1px solid black");
 
-  const barHeight = 30;
+d3.csv("data/tvBrandCount.csv", d => {
+  return {
+    brand: d.brand,
+    count: +d.count
+  };
+}).then(data => {
+  data.sort((a, b) => b.count - a.count);
+  drawBarChart(data);
+});
+
+const drawBarChart = data => {
+  const barHeight = 20;
   const spacing = 10;
-  const startX = 50;
-  const startY = 50;
 
   svg
     .selectAll("rect")
     .data(data)
     .join("rect")
-    .attr("class", d => `bar-${d.count}`)
-    .attr("x", startX)
-    .attr("y", (d, i) => startY + i * (barHeight + spacing))
+    .attr("class", d => `bar bar-${d.count}`)
+    .attr("x", 0)
+    .attr("y", (d, i) => i * (barHeight + spacing))
     .attr("width", d => d.count)
     .attr("height", barHeight)
-    .attr("fill", "steelblue");
+    .attr("fill", "blue");
 };
 
