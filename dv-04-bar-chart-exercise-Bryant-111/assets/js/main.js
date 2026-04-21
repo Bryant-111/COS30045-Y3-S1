6ec6da6ba5e82dcf344a915efb41fc223c16d939
Pasan DeZoyza <104760094@student.swin.edu.au.com>
2026-04-21 15:27:52 +1000

    wbfwiv

--- dv-04-bar-chart-exercise-Bryant-111/assets/js/main.js
+++ dv-04-bar-chart-exercise-Bryant-111/assets/js/main.js
@@ -1,14 +1,41 @@
-const createBarChart = data => {
-  console.log("createBarChart called");
+console.log("MAIN JS LOADED");
 
+const svgWidth = 500;
+const svgHeight = 500;
+const labelSpace = 100;
+
+const svg = d3.select(".responsive-svg-container")
+  .append("svg")
+  .attr("viewBox", `0 0 ${svgWidth} ${svgHeight}`)
+  .style("border", "1px solid black");
+
+d3.csv("data/tvBrandCount.csv", d => {
+  return {
+    brand: d.brand,
+    count: +d.count
+  };
+}).then(data => {
+  console.log("Loaded data:", data);
+  console.log("Length:", data.length);
+  console.log("Max:", d3.max(data, d => d.count));
+  console.log("Min:", d3.min(data, d => d.count));
+  console.log("Extent:", d3.extent(data, d => d.count));
+
+  data.sort((a, b) => b.count - a.count);
+  console.log("Sorted data:", data);
+
+  drawBarChart(data);
+});
+
+const drawBarChart = data => {
   const xScale = d3.scaleLinear()
-    .domain([0, 1200])
-    .range([0, 400]);
+    .domain([0, 1100])
+    .range([0, 350]);
 
   const yScale = d3.scaleBand()
     .domain(data.map(d => d.brand))
     .range([0, svgHeight])
-    .padding(0.2);
+    .padding(0.1);
 
   const barAndLabel = svg
     .selectAll("g")
@@ -18,8 +45,7 @@ const createBarChart = data => {
 
   barAndLabel
     .append("rect")
-    .attr("class", d => `bar bar-${d.count}`)
-    .attr("x", 100)
+    .attr("x", labelSpace)
     .attr("y", 0)
     .attr("width", d => xScale(d.count))
     .attr("height", yScale.bandwidth())
@@ -28,10 +54,18 @@ const createBarChart = data => {
   barAndLabel
     .append("text")
     .text(d => d.brand)
-    .attr("x", 90)
+    .attr("x", labelSpace - 10)
     .attr("y", yScale.bandwidth() / 2)
     .attr("dy", "0.35em")
     .attr("text-anchor", "end")
+    .style("font-size", "13px");
+
+  barAndLabel
+    .append("text")
+    .text(d => d.count)
+    .attr("x", d => labelSpace + xScale(d.count) + 4)
+    .attr("y", yScale.bandwidth() / 2)
+    .attr("dy", "0.35em")
     .style("font-size", "13px");
 };
 
