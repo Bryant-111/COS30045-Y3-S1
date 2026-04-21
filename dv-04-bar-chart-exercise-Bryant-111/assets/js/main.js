219f34b0d7e9dbfec93825f683fcb7518faa8e2b
Pasan DeZoyza <104760094@student.swin.edu.au.com>
2026-04-21 14:37:16 +1000

    final5

--- dv-04-bar-chart-exercise-Bryant-111/assets/js/main.js
+++ dv-04-bar-chart-exercise-Bryant-111/assets/js/main.js
@@ -9,11 +9,21 @@ d3.csv("data/tvBrandCount.csv", d => {
     count: +d.count
   };
 }).then(data => {
+  console.log("Loaded data:", data);
+  console.log("Length:", data.length);
+  console.log("Max:", d3.max(data, d => d.count));
+  console.log("Min:", d3.min(data, d => d.count));
+  console.log("Extent:", d3.extent(data, d => d.count));
+
   data.sort((a, b) => b.count - a.count);
+  console.log("Sorted data:", data);
+
   drawBarChart(data);
 });
 
 const drawBarChart = data => {
+  console.log("drawBarChart called");
+
   const barHeight = 20;
   const spacing = 10;
 
@@ -28,4 +38,5 @@ const drawBarChart = data => {
     .attr("height", barHeight)
     .attr("fill", "blue");
 };
+
 
