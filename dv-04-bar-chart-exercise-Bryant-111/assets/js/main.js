bb71546af854e11a9d102435bb555dcda734728f
Pasan DeZoyza <104760094@student.swin.edu.au.com>
2026-04-21 15:11:59 +1000

    ihhbv

--- dv-04-bar-chart-exercise-Bryant-111/assets/js/main.js
+++ dv-04-bar-chart-exercise-Bryant-111/assets/js/main.js
@@ -1,3 +1,31 @@
+console.log("MAIN JS LOADED");
+
+const svgWidth = 500;
+const svgHeight = 800;
+
+const svg = d3.select(".responsive-svg-container")
+  .append("svg")
+  .attr("viewBox", `0 0 ${svgWidth} ${svgHeight}`)
+  .style("border", "1px solid black");
+
+d3.csv("data/tvBrandCount.csv", d => {
+  return {
+    brand: d.brand,
+    count: +d.count
+  };
+}).then(data => {
+  console.log("Loaded data:", data);
+  console.log("Length:", data.length);
+  console.log("Max:", d3.max(data, d => d.count));
+  console.log("Min:", d3.min(data, d => d.count));
+  console.log("Extent:", d3.extent(data, d => d.count));
+
+  data.sort((a, b) => b.count - a.count);
+  console.log("Sorted data:", data);
+
+  createBarChart(data);
+});
+
 const createBarChart = data => {
   console.log("createBarChart called");
 
@@ -7,7 +35,7 @@ const createBarChart = data => {
 
   const yScale = d3.scaleBand()
     .domain(data.map(d => d.brand))
-    .range([0, 800])
+    .range([0, svgHeight])
     .padding(0.2);
 
   svg
@@ -15,10 +43,11 @@ const createBarChart = data => {
     .data(data)
     .join("rect")
     .attr("class", d => `bar bar-${d.count}`)
-    .attr("x", 100)
+    .attr("x", 0)
     .attr("y", d => yScale(d.brand))
     .attr("width", d => xScale(d.count))
     .attr("height", yScale.bandwidth())
     .attr("fill", "blue");
 };
+
 
