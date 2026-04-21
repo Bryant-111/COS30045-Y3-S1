7a8a04c9c970c77ee660d4fdb0ffa1a07a9d5341
Pasan DeZoyza <104760094@student.swin.edu.au.com>
2026-04-21 14:39:30 +1000

    4.6

--- dv-04-bar-chart-exercise-Bryant-111/assets/js/main.js
+++ dv-04-bar-chart-exercise-Bryant-111/assets/js/main.js
@@ -1,6 +1,6 @@
 const svg = d3.select(".responsive-svg-container")
   .append("svg")
-  .attr("viewBox", "0 0 1200 1600")
+  .attr("viewBox", "0 0 500 1600")
   .style("border", "1px solid black");
 
 d3.csv("data/tvBrandCount.csv", d => {
