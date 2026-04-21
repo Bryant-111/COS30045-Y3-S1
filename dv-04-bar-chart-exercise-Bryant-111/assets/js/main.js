35ccab64c7b13707b53e4cc70ab7ca3632c67084
Pasan DeZoyza <104760094@student.swin.edu.au.com>
2026-04-21 14:45:49 +1000

    scale2

--- dv-04-bar-chart-exercise-Bryant-111/assets/js/main.js
+++ dv-04-bar-chart-exercise-Bryant-111/assets/js/main.js
@@ -1,33 +1,7 @@
-const svg = d3.select(".responsive-svg-container")
-  .append("svg")
-  .attr("viewBox", "0 0 500 1600")
-  .style("border", "1px solid black");
-
-d3.csv("data/tvBrandCount.csv", d => {
-  return {
-    brand: d.brand,
-    count: +d.count
-  };
-}).then(data => {
-  console.log("Loaded data:", data);
-  console.log("Length:", data.length);
-  console.log("Max:", d3.max(data, d => d.count));
-  console.log("Min:", d3.min(data, d => d.count));
-  console.log("Extent:", d3.extent(data, d => d.count));
-
-  data.sort((a, b) => b.count - a.count);
-  console.log("Sorted data:", data);
-
-  createBarChart(data);
-});
-
 const createBarChart = data => {
   const xScale = d3.scaleLinear()
     .domain([0, 1200])
     .range([0, 400]);
-
-  console.log("xScale 0 =", xScale(0));
-  console.log("xScale 1200 =", xScale(1200));
 
   const barHeight = 20;
   const spacing = 10;
